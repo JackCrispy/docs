c9e97bfecb2fd5914c12580d31fb48f9d521f03e
HyRo <generalkubo@gmail.com>
2021-07-01 21:35:51 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "discord-buttons",
-  base: "/docs/",
+  base: "/",
   head: [
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
